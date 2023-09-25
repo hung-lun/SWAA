4665508fc4338bb0ddc482636e622567e1eea283
hung-lun <s97014280@gmail.com>
2023-09-25 09:54:15 +0800

    fix: :heavy_minus_sign: 刪除程式碼 09250954

--- front/js/front-rehabilitation.js
+++
@@ -1,9 +0,0 @@
-var goback = document.getElementById("goback");
-var iframe = document.getElementById("iframe");
-var iframeSrc = iframe.src;
-
-goback.style.display = "none";
-console.log(iframeSrc);
-if (iframeSrc != "front/front-points-button.html"){
-    goback.style.display = "flex";
-}//要改!!
