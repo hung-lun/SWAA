93190e56c879fbf4b5e084e99b08bc35c7e3d013
hung-lun <s97014280@gmail.com>
2023-10-08 20:49:23 +0800

    feat: :art: 新增JS判斷  10082049

--- front/js/front-diagnosis-q03.js
+++ front/js/front-diagnosis-q03.js
@@ -62,3 +62,26 @@ count.innerHTML = "已選"+checkedCount+"/7";
             // window.location.href = "./front-diagnosis-q04.html";
         }
     }
+
+
+    //把上一頁得到的分數加總
+var getUrlString = location.href;  //取得網址
+var url = new URL(getUrlString);  //將網址 (字串轉成URL)
+var aa = url.searchParams.get('contact01');  //取得網址裡01=?
+var bb = url.searchParams.get('contact02');  //取得網址裡02=?
+var cc = url.searchParams.get('contact03');  //取得網址裡03=?
+var dd = url.searchParams.get('contact04');  //取得網址裡04=?
+var ee = url.searchParams.get('contact05');  //取得網址裡05=?
+var ff = url.searchParams.get('contact06');  //取得網址裡06=?
+var gg = url.searchParams.get('contact07');  //取得網址裡07=?
+
+var aa1 = parseInt(aa, 24);
+var bb1 = parseInt(bb, 24);
+var cc1 = parseInt(cc, 24);
+var dd1 = parseInt(dd, 24);
+var ee1 = parseInt(ee, 24);
+var ff1 = parseInt(ff, 24);
+var gg1 = parseInt(gg, 24);
+
+var total = aa1+bb1+cc1+dd1+ee1+ff1+gg1;
+alert(total);
--- front/js/front-index.js
+++ front/js/front-index.js
@@ -1,6 +1,10 @@
 //登出
-var logout1 = document.getElementById("logout2");
+var logout1 = document.getElementById("logout");
+var logout2 = document.getElementById("logout1");  //平板RWD登出按鈕
+var logout3 = document.getElementById("logout2");  //手機RWD登出按鈕
 logout1.addEventListener("click", logout);
+logout2.addEventListener("click", logout);
+logout3.addEventListener("click", logout); 
     function logout() {
         if (confirm('確認要登出嗎？') == true) {
             window.location.href = "../front/login.html";
